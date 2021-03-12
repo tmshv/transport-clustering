01470febef302bff73bbc6ce0a59b91e135c48bd
Roman Timashev <roman@tmshv.ru>
2021-03-12 16:10:09 +0300

    rm brisque from create_image

--- create_dataset.py
+++ create_dataset.py
@@ -1,12 +1,5 @@
 import pandas as pd
 from pathlib import Path
-import imquality.brisque as brisque
-import PIL.Image
-from tqdm import tqdm
-
-# ignore warnings from skimage
-import warnings
-warnings.simplefilter(action='ignore', category=FutureWarning)
 
 df = pd.read_csv('images_paths.csv')
 df.columns = ['path']
@@ -23,15 +16,4 @@ def get_car_id(row):
     return f.parent.name
 df['car_id'] = df.apply(get_car_id, axis=1)
 
-total = df.shape[0]
-for row_id, row, in tqdm(df.iterrows(), total=total):
-    path = row['path']
-    img = PIL.Image.open(path)
-    q = brisque.score(img)
-    w, h = img.size
-    df.loc[row_id, 'width'] = w
-    df.loc[row_id, 'height'] = h
-    df.loc[row_id, 'brisque'] = q
-
-print(df)
 df.to_csv('transport_dataset.csv', index=False)
