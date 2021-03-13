5a776e581eaaeea02782564d74e4ef1f89a7992a
Roman Timashev <roman@tmshv.ru>
2021-03-13 17:48:22 +0300

    notebook updated to final version

--- utils.py
+++ utils.py
@@ -20,7 +20,33 @@ def show_cluster(df, max_images=10):
         i += 1
 
 
+def show_clusters(df, max_images=10, size=25, title=None):
+    i = 0
+    clusters = df.groupby('label')
+    rows = len(clusters.groups.keys())
+    figsize = (int(size * (max_images/rows) * 1), size)
+    plt.figure(figsize=figsize)
+    if title:
+        plt.suptitle(title, size=16)
+    for c in clusters.groups.keys():
+        cdf = clusters.get_group(c)
+        if cdf.shape[0] > max_images:
+            cdf = cdf.sample(max_images)
+        for index, row in cdf.iterrows():
+            plt.subplot(rows, max_images, i+1)
+            file = row['path']
+            img = io.imread(file)
+            img = np.array(img)
+            plt.imshow(img)
+            plt.axis('off')
+            i += 1
+
+
 def get_sample(data, n_items):
     idx = np.random.choice(data.shape[0], n_items, replace=False)
     data[idx, :], idx
 
+
+def calc_clusters_size_std(labels):
+    unique, counts = np.unique(labels, return_counts=True)
+    return counts.std()
